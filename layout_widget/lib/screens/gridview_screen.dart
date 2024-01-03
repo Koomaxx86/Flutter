26f3b27281b942e922095c8894be9e3c72015ae6
Koomaxx86 <bluejjw86@daum.net>
2024-01-03 10:40:54 +0900

    0103 1040

---
+++ layout_widget/lib/screens/gridview_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class GridViewScreen extends StatefulWidget {
+  const GridViewScreen({super.key});
+
+  @override
+  State<GridViewScreen> createState() => _GridViewScreenState();
+}
+
+class _GridViewScreenState extends State<GridViewScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: const Text(
+        '그리드 뷰',
+      )),
+      body: Container(
+        child: Center(
+          child: GridView(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: 10.0,
+              mainAxisSpacing: 10.0,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
